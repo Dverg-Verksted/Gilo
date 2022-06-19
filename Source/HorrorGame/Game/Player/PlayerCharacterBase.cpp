cc40de25433803707dcc4d0a20f411ac09e0a66e
Trofimov Yuri <on_zdes@mail.ru>
2022-06-19 16:40:29 +0300

    PlayerController
    Добавил класс PlayerController-а и подключил его к GameMode
    Добавил к проекту плагин EnhancedInput
    Создал action-ы и дефолтный контекст для EnhancedInput. Настроил ввод с клавиатуры/мыши
    Настроил механику плавного наклона влево/вправо

---
+++ Source/HorrorGame/Game/Player/PlayerCharacterBase.cpp
@@ -0,0 +1,210 @@
+﻿// It is owned by the company Dverg Verksted.
+
+
+#include "Game/Player/PlayerCharacterBase.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerCharacterBase::APlayerCharacterBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	MaxPeekSlowDown = 0.3f;
+	PeekAlpha = 0.0f;
+	PeekState = Default;
+
+	PeekLeftOffset = FVector(0.0f, -15.0f, 60.0f);
+	PeekRightOffset = FVector(0.0f, 15.0f, 60.0f);
+	PeekRotation = 20.0f;
+
+	MainCameraBoom = ObjectInitializer.CreateDefaultSubobject<USpringArmComponent>(this, TEXT("MainCameraBoom"));
+	if (MainCameraBoom)
+	{
+		MainCameraBoom->SetupAttachment(GetRootComponent());
+		MainCameraBoom->bUsePawnControlRotation = true;
+		MainCameraBoom->bEnableCameraRotationLag = true;
+		MainCameraBoom->TargetArmLength = 0.0f;
+	}
+
+	MainCamera = ObjectInitializer.CreateDefaultSubobject<UCameraComponent>(this, TEXT("MainCamera"));
+	if (MainCamera)
+	{
+		MainCamera->SetupAttachment(MainCameraBoom);
+		MainCamera->bUsePawnControlRotation = false;
+	}
+}
+
+void APlayerCharacterBase::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	MainCameraBoom->SetRelativeTransform(CameraDefaultTransform);
+}
+
+void APlayerCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (PeekCurve)
+	{
+		float PeekCurveMinTime = 0.0f;
+		float PeekCurveMaxTime = 0.0f;
+		PeekCurve->GetTimeRange(PeekCurveMinTime, PeekCurveMaxTime);
+		FOnTimelineFloat PeekTimelineProgress;
+		PeekTimelineProgress.BindUFunction(this, FName("PeekTimelineProgress"));
+		PeekTimeline.AddInterpFloat(PeekCurve, PeekTimelineProgress);
+
+		FOnTimelineEvent PeekTimelineFinish;
+		PeekTimelineFinish.BindUFunction(this, FName("PeekTimelineFinished"));
+		PeekTimeline.SetTimelineFinishedFunc(PeekTimelineFinish);
+
+		PeekTimeline.SetLooping(false);
+		PeekTimeline.SetTimelineLength(PeekCurveMaxTime);
+	}
+}
+
+void APlayerCharacterBase::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+
+	if (const auto* PC = Cast<APlayerController>(GetController()))
+	{
+		if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->ClearAllMappings();
+			if (DefaultInputContext)
+				Subsystem->AddMappingContext(DefaultInputContext, 0);
+		}
+	}
+}
+
+void APlayerCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	PeekTimeline.TickTimeline(DeltaTime);
+}
+
+void APlayerCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (auto* PlayerEnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction)
+			PlayerEnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacterBase::MoveActionHandler);
+
+		if (LookAction)
+			PlayerEnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacterBase::LookActionHandler);
+
+		if (PeekAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(PeekAction, ETriggerEvent::Started, this, &APlayerCharacterBase::PeekActionHandler);
+			PlayerEnhancedInputComponent->BindAction(PeekAction, ETriggerEvent::Triggered, this, &APlayerCharacterBase::PeekActionHandler);
+			PlayerEnhancedInputComponent->BindAction(PeekAction, ETriggerEvent::Completed, this, &APlayerCharacterBase::PeekStopHandler);
+		}
+
+		if (CrouchAction)
+			PlayerEnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this,
+				&APlayerCharacterBase::CrouchActionHandler);
+	}
+}
+
+void APlayerCharacterBase::MoveActionHandler(const FInputActionValue& ActionValue)
+{
+	FVector InputAxis = ActionValue.Get<FVector>();
+	if (PeekAlpha != 0.0f)
+	{
+		const float PeekSlowDownAmount = FMath::Abs(PeekAlpha) * MaxPeekSlowDown;
+		InputAxis.X = InputAxis.X * (1.0f - PeekSlowDownAmount);
+		InputAxis.Y = InputAxis.Y * (1.0f - PeekSlowDownAmount);
+	}
+	AddMovementInput(GetActorForwardVector(), InputAxis.X);
+	AddMovementInput(GetActorRightVector(), InputAxis.Y);
+}
+
+void APlayerCharacterBase::LookActionHandler(const FInputActionValue& ActionValue)
+{
+	const FVector InputAxis = ActionValue.Get<FVector>();
+	AddControllerPitchInput(InputAxis.Y);
+	AddControllerYawInput(InputAxis.X);
+}
+
+void APlayerCharacterBase::PeekActionHandler(const FInputActionValue& ActionValue)
+{
+	const float Magnitude = ActionValue.Get<float>();
+	if (Magnitude > 0)
+	{
+		if (PeekState == PeekLeft)
+			return;
+	}
+	else if (Magnitude < 0)
+	{
+		if (PeekState == PeekRight)
+			return;
+	}
+
+	if (PeekState == Default)
+	{
+		PeekState = Magnitude > 0 ? PeekRight : PeekLeft;
+		PeekTimeline.PlayFromStart();
+		UE_LOG(LogTemp, Log, TEXT("Timeline started PeekState %d"), PeekState);
+	}
+	else
+	{
+		if (PeekTimeline.IsReversing())
+			PeekTimeline.Play();
+	}
+}
+
+void APlayerCharacterBase::PeekStopHandler(const FInputActionValue& ActionValue)
+{
+	if (PeekState != Default)
+		PeekTimeline.Reverse();
+}
+
+void APlayerCharacterBase::PeekTimelineProgress(float Value)
+{
+	if (PeekState == Default)
+		return;
+
+	PeekAlpha = Value;
+
+	const FVector PeekLocation = PeekState == PeekRight ? PeekRightOffset : PeekLeftOffset;
+	const FVector TargetLocation = PeekLocation;
+	const FVector StartLocation = CameraDefaultTransform.GetLocation();
+	const FVector CameraLocation = FMath::Lerp(StartLocation, TargetLocation, Value);
+	MainCameraBoom->SetRelativeLocation(CameraLocation);
+
+	if (auto* PC = Cast<APlayerController>(GetController()))
+	{
+		FRotator ControlRotation = PC->GetControlRotation();
+		ControlRotation.Roll = Value * PeekRotation * (PeekState == PeekRight ? 1.0f : -1.0f);
+		PC->SetControlRotation(ControlRotation);
+	}
+}
+
+void APlayerCharacterBase::PeekTimelineFinished()
+{
+	if (PeekTimeline.GetPlaybackPosition() == 0.0f)
+	{
+		// Если таймлайн вернулся в исходную позицию сбрасываем состояние отклонения
+		PeekState = Default;
+		PeekAlpha = 0.0f;
+		UE_LOG(LogTemp, Log, TEXT("Timeline finished PeekState %d"), PeekState);
+	}
+}
+
+void APlayerCharacterBase::CrouchActionHandler(const FInputActionValue& ActionValue)
+{
+	if (auto* CharMovement = GetCharacterMovement())
+	{
+		if (CharMovement->IsCrouching())
+		{
+			CharMovement->UnCrouch();
+		}
+		else
+		{
+			CharMovement->Crouch();
+		}
+	}
+}
---
+++ Source/HorrorGame/Game/Player/PlayerCharacterBase.h
@@ -0,0 +1,131 @@
+﻿// It is owned by the company Dverg Verksted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "GameplayTagAssetInterface.h"
+#include "Camera/CameraComponent.h"
+#include "Components/TimelineComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "PlayerCharacterBase.generated.h"
+
+/** Состояние наклона персонажа */
+UENUM()
+enum EPlayerPeekState
+{
+	Default,
+	PeekLeft,
+	PeekRight,
+};
+
+UCLASS()
+class HORRORGAME_API APlayerCharacterBase : public ACharacter, public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacterBase(const FObjectInitializer& ObjectInitializer);
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCameraComponent> MainCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USpringArmComponent> MainCameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayTags",
+		Meta = (DisplayName = "GameplayTags", ExposeOnSpawn = true), SaveGame)
+	FGameplayTagContainer GameplayTags;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FTransform CameraDefaultTransform;
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FVector PeekLeftOffset;
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FVector PeekRightOffset;
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	float PeekRotation;
+
+	UPROPERTY(EditDefaultsOnly, DisplayName="Ввод ходьбы", Category="Input", AdvancedDisplay)
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, DisplayName="Ввод камеры", Category="Input", AdvancedDisplay)
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditDefaultsOnly, DisplayName="Ввод наклона", Category="Input", AdvancedDisplay)
+	TObjectPtr<UInputAction> PeekAction;
+
+	UPROPERTY(EditDefaultsOnly, DisplayName="Ввод приседа", Category="Input", AdvancedDisplay)
+	TObjectPtr<UInputAction> CrouchAction;
+
+	UPROPERTY(EditDefaultsOnly, DisplayName="Контекст ввода", Category="Input", meta=(ToolTip="Контекст ввода по-умолчанию"),
+		AdvancedDisplay)
+	TObjectPtr<UInputMappingContext> DefaultInputContext;
+
+	/** Макс. величина замедления перемещения при наклоне */
+	UPROPERTY(EditDefaultsOnly, DisplayName="Замедление при наклоне", Category="Input",
+		meta=(ToolTip="Макс. величина замедления перемещения при наклоне", ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"
+		), AdvancedDisplay)
+	float MaxPeekSlowDown;
+
+	FTimeline PeekTimeline;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCurveFloat> PeekCurve;
+	
+	EPlayerPeekState PeekState;
+	
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void BeginPlay() override;
+
+	virtual void PawnClientRestart() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override
+	{
+		TagContainer = GameplayTags;
+	}
+
+private:
+	/** Текущая альфа наклона От 0 до 1 */
+	float PeekAlpha;
+
+	/** Обработчик ввода ходьбы */
+	UFUNCTION()
+	void MoveActionHandler(const FInputActionValue& ActionValue);
+
+	/** Обработчик ввода поворота камеры */
+	UFUNCTION()
+	void LookActionHandler(const FInputActionValue& ActionValue);
+	
+	/** Обработчик ввода наклона */
+	UFUNCTION()
+	void PeekActionHandler(const FInputActionValue& ActionValue);
+
+	/** Обработчик завершения наклона */
+	UFUNCTION()
+	void PeekStopHandler(const FInputActionValue& ActionValue);
+
+	UFUNCTION()
+	void PeekTimelineProgress(float Value);
+
+	UFUNCTION()
+	void PeekTimelineFinished();
+
+	/** Обработчик ввода приседа */
+	UFUNCTION()
+	void CrouchActionHandler(const FInputActionValue& ActionValue);
+};
